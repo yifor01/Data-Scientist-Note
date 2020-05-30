c092128eeb6a44594856a5be919845adf66faa76
yifor01 <howard99664411@gmail.com>
2020-05-30 11:48:35 +0800

    update

--- python template/request_diff.py
+++ python template/request_diff.py
@@ -1,10 +1,11 @@
 import time
 import asyncio
-import requests
+from p_tqdm import p_map
 
 '''
 比較直接request與透過asyncio request速度差異
 '''
+
 
 def now(): return time.time()
 
@@ -14,7 +15,10 @@ test_url = r'http://share.dmhy.org/topics/list'
 MAX_PAGE = 30
 
 # method 1
+
+
 def get_web_page(url):
+    import requests
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                       'Chrome/81.0.4044.113 Safari/537.36', }
@@ -26,7 +30,10 @@ def get_web_page(url):
         return resp.text
 
 # method 2
+
+
 async def send_req(url):
+    import requests
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                       'Chrome/81.0.4044.113 Safari/537.36', }
@@ -42,15 +49,21 @@ res1 = []
 for page in range(1, MAX_PAGE):
     _res = get_web_page(f'{test_url}/{page}')
     res1.append(_res)
-print(f'Method 1 using time: {now() - start}')  # 28s
+print(f'Method 1 using time: {now() - start}')  # 19.37s
 
 # method 2
 start = now()
 loop = asyncio.get_event_loop()
-tasks = []
+res2 = []
 for page in range(1, MAX_PAGE):
     url = f'/{page}'
     task = loop.create_task(send_req(f'{test_url}/{page}'))
-    tasks.append(task)
-loop.run_until_complete(asyncio.wait(tasks))
-print(f'Method 2 using time: {now() - start}')  # 3s
+    res2.append(task)
+loop.run_until_complete(asyncio.wait(res2))
+print(f'Method 2 using time: {now() - start}')  # 1.16s
+
+# method 3
+start = now()
+res3 = p_map(get_web_page, [test_url + '/' + str(i)
+                            for i in range(1, MAX_PAGE)])
+print(f'Method 3 using time: {now() - start}')  # 2.56s
